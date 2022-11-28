78cbe1343853454c1783c8b2b8350089e5fa95ef
8bit_please <68927773+JustNaimoR@users.noreply.github.com>
2022-11-28 19:20:52 +0300

    IntegerAdvancedTask - completed

--- src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
+++ src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
@@ -10,7 +10,13 @@ public class HomeworkTask {
      * Считаем, что функция определена на всем пространстве от a до b
      */
     public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta) {
-        return 0;
+        double value = 0;
+
+        for (double i = a; i < b; i += delta) {
+            value += delta * function.applyAsDouble(i);
+        }
+
+        return value;
     }
 
     /**
@@ -18,16 +24,38 @@ public class HomeworkTask {
      * выводим номер первой максимальной цифры (если их несколько)
      */
     public static byte maxNumber(long a) {
-        return 0;
-    }
+        /*   Math.abs(Long.MIN_VALUE) == Long.MIN_VALUE
+        *      (*) Отрицательное значение будет равно себе же в модуле (*)
+        */
 
+        String stringA = Long.toString(a);
+        byte step = 0;
+        char max = '0' - 1;
+        byte maxNum = 0;
+
+        for (char c: stringA.toCharArray()) {
+            if (c == '-') {
+                continue;
+            }
+            step++;
+
+            if (max < c) {
+                maxNum = step;
+                max = c;
+            }
+        }
+
+        return maxNum;
+    }
 
     /**
      * Даны две точки в пространстве (x1, y1) и (x2, y2). Вам нужно найти Y координату третьей точки (x3, y3),
      * которая находится на той же прямой что и первые две.
      */
     public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        return 0;
+        // Направляющий вектор - a(x1 - x2, y1 - y2)
+        // Каноническое уравнение прямой: (x - x1) / Ax = (y - y1) / Ay
+        return (double) (y1 - y2) / (x1 - x2) * (x3 - x1) + y1;
     }
 
     /**
@@ -36,7 +64,14 @@ public class HomeworkTask {
      * Это дополнительное задание, необязательное для выполнения
      */
     public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        return 0;
+        double AB = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+        double BC = Math.sqrt(Math.pow(x2 - x3, 2) + Math.pow(y2 - y3, 2));
+        double CD = Math.sqrt(Math.pow(x3 - x4, 2) + Math.pow(y3 - y4, 2));
+        double AD = Math.sqrt(Math.pow(x4 - x1, 2) + Math.pow(y4 - y1, 2));
+
+        double p = (AB + BC + CD + AD) / 2;
+
+        return Math.sqrt((p - AB) * (p - BC) * (p - CD) * (p - AD));
     }
 
 }
