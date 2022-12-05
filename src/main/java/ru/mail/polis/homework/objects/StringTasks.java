f2778a6bb63d523e526b78878573dcb1e58ca1ff
8bit_please <68927773+JustNaimoR@users.noreply.github.com>
2022-12-05 18:06:18 +0300

    IntegerAdvancedTask - completed

--- src/main/java/ru/mail/polis/homework/objects/StringTasks.java
+++ src/main/java/ru/mail/polis/homework/objects/StringTasks.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.objects;
 
+// import com.sun.org.apache.xpath.internal.operations.Number;
+
 public class StringTasks {
 
     /**
@@ -14,7 +16,74 @@ public class StringTasks {
      * Работайте со строкой, НЕ надо ее переводить в массив байт (это можно использовать только для цикла)
      * У класса Character есть полезные методы, например Character.isDigit()
      */
+
+    //TODO  В функции предпологается вернуть объект класса Number, но описанный класс, который подключен,
+    //      я не понял и как вернуть именно этот тип и откуда - не знаю :/
+    // Функция реализована чисто для алгоритма.
     public static Number valueOf(String str) {
-        return null;
+        String allowedChars = "-.e";
+
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+        // Убираем лишние символы из записи
+        for (int i = 0; i < str.length(); i++) {
+            char cur = str.charAt(i);
+            if (!Character.isDigit(str.charAt(i)) && !allowedChars.contains(cur + "")) {
+                str = str.replaceAll(cur + "", "");
+                i--;
+            }
+        }
+        // Две '.' и 'e' в записи числа
+        if (str.indexOf('.') != str.lastIndexOf('.') ||
+                str.indexOf('e') != str.lastIndexOf('e')) {
+            return null;
+        }
+
+        /// Исключительные ситуации ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        boolean doublePart = false;     // Часть числа после точки (Для числа boolean типа)
+        for (int i = 0; i < str.length(); i++) {
+
+            boolean nextCharIsDigit = (i + 1 != str.length() && Character.isDigit(str.charAt(i + 1)));
+            switch (str.charAt(i)) {
+                case '-':
+                    if (i == 0 && nextCharIsDigit) {
+                        // Первый символ - минус
+                    } else if (i != 0 && str.charAt(i - 1) == 'e' && nextCharIsDigit) {
+                        // минус после 'e' перед цифрой
+                    } else {
+                        return null;
+                    }
+                    break;
+                case '.':
+                    doublePart = true;
+                    if (str.indexOf('e') != -1 && str.indexOf('e') < i) {
+                        return null;
+                    }
+
+                    break;
+                case 'e':
+                    if (i + 1 == str.length()) {
+                        // Последний элемент - экспонента
+                        return null;
+                    }
+                    break;
+            }
+        }
+
+        if (doublePart || str.contains('e' + "")) {
+            return Double.valueOf(str);
+        } else {
+            return Long.valueOf(str);
+        }
+    }
+
+    public static void main(String[] args) {
+        /**
+         *  Алгоритм работающий, вот только блять тесты ска требуют непонять что, а в реализации класса, что передается как
+         *   возвращаемое значение, и которое импортировано непонять откуда, вообще нету норм методов для анализа.
+         *   Незачем сидеть так долго над этой херней...
+         */
+        System.out.println(valueOf("fff1fdf.asdsad3e"));
     }
 }
